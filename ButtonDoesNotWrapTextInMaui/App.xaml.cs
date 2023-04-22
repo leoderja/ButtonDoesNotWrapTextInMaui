54be45759149b0b759db61cc93947e85914f9e90
Leonardo Der Jachadurian <5513105+leoderja@users.noreply.github.com>
2023-04-22 20:28:01 -0300

    Add project files.

---
+++ ButtonDoesNotWrapTextInMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ButtonDoesNotWrapTextInMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ButtonDoesNotWrapTextInMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ButtonDoesNotWrapTextInMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
